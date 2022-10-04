c1d639dfd759d6732a74609765edbdb479a839ac
kartik-ramani <67691961+kartik-ramani@users.noreply.github.com>
2022-10-04 14:57:26 +0530

    Add files via upload
    Deployer Module Uploaded

---
+++ DeployerModule/main.tf
@@ -0,0 +1,84 @@
+
+// Security Group
+
+resource "aws_security_group" "sg" {
+  name        = "securitygroup"
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+// KEY PAIR GENRATES PEM FILE
+
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = var.keyName
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./${var.keyName}.pem && chmod 400 ${var.keyName}.pem"
+  }
+}
+
+
+
+// EC2 SetUp
+
+resource "aws_instance" "app_server" {
+  ami                    = var.ami
+  instance_type          = var.instanceType
+  key_name               = aws_key_pair.kp.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = "Terraform "
+  }
+
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${var.keyName}.pem")
+    host        = self.public_ip
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt upgrade - y",
+      "sudo apt install docker.io -y && sudo docker run -dp ${var.port}:80 ${var.dockerImage}"
+    ]
+
+  }
+
+
+  depends_on = [
+    aws_key_pair.kp
+  ]
+}
+
+
---
+++ DeployerModule/variable.tf
@@ -0,0 +1,71 @@
+/**  
+    @Description - Default configuration for all types of requirement 
+    @Parameters
+        keyName - KeyName of Pem file which requires to connect the EC2 Via SSH
+        port - Open the Port of EC2 according the Project default is 80
+        isPrivate - Either Github or Docker image is Private or Not Default is false
+        region - 
+        ami - 
+
+**/
+variable "keyName" {
+  default = "mykey"
+}
+
+variable "port" {
+  default = 80
+}
+
+variable "isPrivate" {
+  default = false
+}
+
+variable "ami" {
+  default = "ami-08df94af6199f15b6"
+}
+
+variable "instanceType" {
+  default = "t2.micro"
+}
+
+
+variable "remoteInline" {
+  type = list(string)
+  default =  [
+      "sudo apt update",
+      "sudo apt upgrade -y",
+      "sudo apt-get install docker.io -y && sudo docker run -dp 80:80 docker/getting-started"
+    ]
+  }
+
+/** 
+    @Description - Configuration variable for the Docker Image
+    @Parameters 
+        dockerImage - Actual Image name uploaded to DockerHub
+        userName - 
+
+    @Commands [{
+        loginDocker - docker login --username={maryatdocker} --email=mary@docker.com
+    }]
+
+**/
+variable "dockerImage" {
+  default = "docker/getting-started"
+}
+
+variable "dockerUserName" {
+  default = null
+}
+
+variable "dockerPassword" {
+  default = null
+}
+
+/** 
+        @Description - Configuration variable for the Github
+
+**/
+
+variable "gitUrl" {
+  default = "https://github.com/docker/getting-started.git"
+}
